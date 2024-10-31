2d75041c29adff2d838c122c43a168e7b63de1ea
Felipe Gómez <feligomez160@gmail.com>
2024-10-31 14:48:39 -0500

    yulius10
    ejemplo basico de google maps y ionic

--- prueba-maps/src/app/maps/home/home.module.ts
+++ prueba-maps/src/app/maps/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {GoogleMap,MapAdvancedMarker,MapInfoWindow} from '@angular/google-maps'
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +14,15 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    GoogleMap,
+    MapAdvancedMarker,
+    MapInfoWindow,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule {
+
+  
+
+}
--- prueba-maps/src/app/maps/home/home.page.ts
+++ prueba-maps/src/app/maps/home/home.page.ts
@@ -1,19 +1,30 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, viewChild, ViewChild, viewChildren } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import {GoogleMap, MapAdvancedMarker, MapInfoWindow} from '@angular/google-maps';
+
 import type { Locations } from 'src/app/models/location.models';
 import { LocationsService } from 'src/app/services/locations.service';
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  
 })
 export class HomePage implements OnInit {
+  
+  infoWindowRef = viewChild.required(MapInfoWindow);
+  markerRef = viewChildren(MapAdvancedMarker);
 
   readonly #locationsService = inject(LocationsService);
 
-  locations$ = this.#locationsService.getAllLocations({lat:4.6482784,lng:-74.272319});
+  center = signal<google.maps.LatLngLiteral>({lat:4.6482784,lng:-74.272319});
+  zoom = signal(12);
+
+
+
+  locations$ = this.#locationsService.getAllLocations(this.center());
   $locations = toSignal(this.locations$,{
     initialValue: [],
   });
@@ -21,4 +32,18 @@ export class HomePage implements OnInit {
   ngOnInit() {
   }
 
+
+  mapClick(location:Locations,market:MapAdvancedMarker){
+    console.log(location);
+    const content = `<h1 class="font-bold text-2x1" style="color:black"> ${location.name} </h1>
+    <p style="color:black"> ${location.description} </p>`;
+    this.infoWindowRef().open(market,false,content);
+  }
+
+  goToPlace(location:Locations,position:number){
+    const markers = this.markerRef();
+    const markerRef = markers[position];
+    this.mapClick(location,markerRef);
+  }
 }
+//AIzaSyC6m39pUDbKTEU2G0Lb40wWlCRpuY_qTsI
